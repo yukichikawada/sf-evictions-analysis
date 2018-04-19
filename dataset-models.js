26693695b3fa04dff9ffeecc74b6615628d1b2b0
Yukichi Kawada <yukichikawada@gmail.com>
2018-04-18 22:27:59 -0700

    Add count to year model

--- dataset-models.js
+++ dataset-models.js
@@ -39,17 +39,17 @@ const pieCharts = { // 20 objects
   }
 }
 
-function neighborhoodTypes(evictions) {
-  let i = 0;
-  const neighborhoods = [];
-  for (i; i < evictions.length; i++) {
-    if (!neighborhoods.includes(evictions[i]["neighborhood"])) {
-      neighborhoods.push(evictions[i]["neighborhood"]);
-    }
-  }
-
-  return neighborhoods;
-}
+// function neighborhoodTypes(evictions) {
+//   let i = 0;
+//   const neighborhoods = [];
+//   for (i; i < evictions.length; i++) {
+//     if (!neighborhoods.includes(evictions[i]["neighborhood"])) {
+//       neighborhoods.push(evictions[i]["neighborhood"]);
+//     }
+//   }
+//
+//   return neighborhoods;
+// }
 
 function countOfNeighborhoodBlanks(evictions) {
   let i  = 0;
@@ -69,7 +69,10 @@ function yearObj(evictions) {
     let lastIndex = years.length - 1;
     if (i === 0 || years[lastIndex]["year"] === evictions[i]["year"]) { // same year
       if (i === 0) {
-        const firstYear = {"year": evictions[i]["year"]};
+        const firstYear = {
+          "year": evictions[i]["year"],
+          "count": 1
+        };
         Object.keys(evictions[i]).map(key => {
           if (evictions[i][key] === "TRUE") {
             firstYear[key] = 1;
@@ -81,14 +84,19 @@ function yearObj(evictions) {
           if (evictions[i][key] === "TRUE") {
             if (years[lastIndex][key]) {
               years[lastIndex][key]++;
+              years[lastIndex]["count"]++;
             } else {
               years[lastIndex][key] = 1;
+              years[lastIndex]["count"] = 1;
             }
           }
         });
       }
     } else { // different year
-      const year = {"year": evictions[i]["year"]};
+      const year = {
+        "year": evictions[i]["year"],
+        "count": 1
+      };
       Object.keys(evictions[i]).map(key => {
         if (evictions[i][key] === "TRUE") {
           year[key] = 1;
@@ -100,17 +108,17 @@ function yearObj(evictions) {
   console.log(years);
   return years;
 }
-
-function yearIterator(evictions) {
-  const years = [];
-  for (var i = 0; i < evictions.length; i++) {
-    if (!years.includes(evictions[i]["year"])) {
-      years.push(evictions[i]["year"]);
-    }
-  }
-
-  return years; // 22 years (1997...2018)
-}
+//
+// function yearIterator(evictions) {
+//   const years = [];
+//   for (var i = 0; i < evictions.length; i++) {
+//     if (!years.includes(evictions[i]["year"])) {
+//       years.push(evictions[i]["year"]);
+//     }
+//   }
+//
+//   return years; // 22 years (1997...2018)
+// }
 
 const neighborhoods = [
   "Outer Richmond", "Inner Sunset", "Mission", "Western Addition",
