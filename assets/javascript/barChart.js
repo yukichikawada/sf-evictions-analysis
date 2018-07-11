6d8b4be1dd67c6a2181257bea0735b5a52b46611
Yukichi Kawada <yukichikawada@gmail.com>
2018-07-10 23:59:13 -0700

    Try a different approach to getting axis right

--- assets/javascript/barChart.js
+++ assets/javascript/barChart.js
@@ -18,59 +18,104 @@ const neighborhoodNames = [
   "West of Twin Peaks", "Western Addition"
 ]; // 41 count
 
-const height2 = 700;
-const width2 = 700;
-const width2Margin = 50;
-const height2Margin = 50;
+const fullHeight2 = 700;
+const fullWidth2 = 700;
+const margin2 = {top: 20, right: 30, bottom: 20, left: 30};
+const width2 = fullWidth2 - margin2.left - margin2.right;
+const height2 = fullHeight2 - margin2.top - margin2.bottom;
 
 const barYear = yearRange[0];
 
-function createBarChart(width, height) {
+function createBarChart(width, height, year) {
   let barChart = d3.select('#barchart')
-                      .attr('width', width - width2Margin)
-                      .attr('height', height - height2Margin);
+                      .attr('width', width)
+                      .attr('height', height);
 
-  const xBar = d3.scaleBand().rangeRound([0, width]).padding(0.1);
-  const yBar = d3.scaleLinear().rangeRound([height, 0]);
+  const data = neighborhoodNames.filter(hood => hood.year === year);
 
-  let groupBarChart = barChart.append('g')
-                          .attr('transform', 'translate(', (width2Margin / 2) + ',' + (height2Margin / 2) + ')');
+  barChart.append('g')
+    .attr('transform', 'translate(' + margin2.left + ',' + margin2.top + ')');
 
-  const barData = hoods.filter(obj => obj.year === barYear);
-
-  xBar.domain(barData.map(d => d.neighborhood));
-  yBar.domain([0, d3.max(barData, d => d.count)]);
-
-  groupBarChart.append('g')
-                  .attr('class', 'axis axis--y')
-                  .attr('transform', 'translate(0', + height + ')')
-                  .call(d3.axisBottom(xBar));
-
-  groupBarChart.append('g')
-                  .attr('class', 'axis axis--y')
-                  .call(d3.axisLeft(yBar).ticks(15))
-                .append('text')
-                  .attr('transform', 'rotate(-90)')
-                  .attr('y', 6)
-                  .attr('dy', '0.71em')
-                  .attr('text-anchor', 'end')
-                  .text('Frequency');
+  const xScale2 = d3.scaleBand()
+                    .domain(neighborhoodNames)
+                    .range([0, width])
+                    .paddingInner(0.1);
   
-  groupBarChart.selectAll('.bar')
-    .data(barData)
-    .enter().append('rect')
-      .attr('class', 'bar')
-      .attr('x', d => xBar(d.neighborhood))
-      .attr('y', d => yBar(d.count))
-      .attr('width', xBar.bandwidth())
-      .attr('height', d => height - yBar(d.count));
+  const yScale2 = d3.scaleLinear()
+                    .domain([0, d3.max(data, d => d.count)])
+                    .range([height, 0])
+                    .nice();
+
+  const barWidth = xScale2.bandwidth();
+
+  let bars = barChart.append('g')
+                      .classed('bars', true);
+
+  bars.selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+        .classed('bar', true)
+        .attr('x', (d, i) => xScale2(d.neighborhood))
+        .attr('width', barWidth)
+        .attr('y', d => yScale2(d.count))
+        .attr('height', d => height - yScale2(d.count));
+
+  xAxis2 = d3.axisBottom(xScale2)
+              .tickSizeOuter(0);
+  yAxis2 = d3.axisLeft(yScale2);
+
+  barChart.append('g')
+          .classed('x axis', true)
+          .attr('transform', 'translate(0', + height + ')')
+          .class(xAxis2);
+
+  barChart.append('g')
+          .classed('y axis', true)
+          .call(yAxis2);
 }
 
+//   const xBar = d3.scaleBand().rangeRound([0, width]).padding(0.1);
+//   const yBar = d3.scaleLinear().rangeRound([height, 0]);
+
+//   let groupBarChart = barChart.append('g')
+//                           .attr('transform', 'translate(', (width2Margin / 2) + ',' + (height2Margin / 2) + ')');
+
+//   const barData = hoods.filter(obj => obj.year === barYear);
+
+//   xBar.domain(barData.map(d => d.neighborhood));
+//   yBar.domain([0, d3.max(barData, d => d.count)]).range([height - 25, 25]);
+
+//   groupBarChart.append('g')
+//                   .attr('class', 'axis axis--y')
+//                   .attr('transform', 'translate(0', + height + ')')
+//                   .call(d3.axisBottom(xBar));
+
+//   groupBarChart.append('g')
+//                   .attr('class', 'axis axis--y')
+//                   .call(d3.axisLeft(yBar).ticks(10))
+//                 .append('text')
+//                   .attr('transform', 'rotate(-90)')
+//                   .attr('y', 6)
+//                   .attr('dy', '0.71em')
+//                   .attr('text-anchor', 'end')
+//                   .text('Frequency');
+  
+//   groupBarChart.selectAll('.bar')
+//     .data(barData)
+//     .enter().append('rect')
+//       .attr('class', 'bar')
+//       .attr('x', d => xBar(d.neighborhood))
+//       .attr('y', d => yBar(d.count))
+//       .attr('width', xBar.bandwidth())
+//       .attr('height', d => height - yBar(d.count));
+// }
 
 
 
 
-createBarChart(width2, height2);
+
+createBarChart(width2, height2, 1997);
 
 
 
