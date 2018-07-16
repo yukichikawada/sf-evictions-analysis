24f426bf0a23b7630e6a3063d9c17a70208f502b
Yukichi Kawada <yukichikawada@gmail.com>
2018-07-15 17:50:22 -0700

    Add tooltip to line chart

--- assets/javascript/lineChart.js
+++ assets/javascript/lineChart.js
@@ -4,13 +4,19 @@ const linechartWidth = 700;
 // const width2 = fullWidth2 - margin2.left - margin2.right;
 // const height2 = fullHeight2 - margin2.top - margin2.bottom;
 
-function lineChart(width, height) {
+const dataLess2018 = years.filter(d => d.year !== 2018);
+
+function lineChart(width, height, data) {
   const xScale = d3.scaleLinear()
                     .domain(d3.extent(years, d => d.year))
                     .range([0, width]);
 
+  // extract max y so domain of y is 0 to max
+  // instead noramlizing graph to min y val thru max y val
+  const maxY = d3.max(data, d => d.count);
+
   const yScale = d3.scaleLinear()
-                    .domain(d3.extent(years, d => d.count))
+                    .domain([0, maxY])
                     .range([height, 0]);
 
   const line = d3.line()
@@ -19,6 +25,7 @@ function lineChart(width, height) {
 
   let svg = d3.select('#linechart')
               .append('svg')
+              .classed('linechartsvg', true)
               .attr('width', width + margin2.left + margin2.right)
               .attr('height', height + margin2.top + margin2.bottom)
                 .append('g')
@@ -49,9 +56,32 @@ function lineChart(width, height) {
       .attr('class', 'dot')
       .attr('cx', d => xScale(d.year))
       .attr('cy', d => yScale(d.count))
-      .attr('r', 5);
+      .attr('r', 5)
+      .attr('fill', 'steelblue');
 }
 
-lineChart(linechartWidth, linechartHeight);
+lineChart(linechartWidth, linechartHeight, years);
 
-// console.log(d3.extent(years, d => d.year))
+function lineTooltip() {
+  const tooltip = d3.select('#lineTooltip');
+  const tgt = d3.select(d3.event.target);
+  const isPoint = tgt.classed('dot');
+  let data;
+  if (isPoint) {
+    data = tgt.data()[0];
+  }
+  
+  tooltip.style('opacity', +isPoint)
+          .style('left', (d3.event.pageX - tooltip.node().offsetWidth / 2) + 'px')
+          .style('top',  (d3.event.pageY - tooltip.node().offsetHeight - 10) + 'px');
+
+  if (data) {
+    tooltip.html(`
+                 <p>Count: ${data.count}</p>
+                 <p>Year: ${data.year}</p>
+                 `)
+  }
+}
+
+d3.selectAll('#linechart')
+  .on('mousemove touchmove', lineTooltip);
